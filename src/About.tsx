f83b4f059113b9ad4b4aa190d5dc298a30d2f690
snappercayt <63928708+snappercayt@users.noreply.github.com>
2021-08-23 20:01:45 +0530

    added pico8 dino game

--- src/About.tsx
+++ src/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About: React.FC = () => (
-  <h1>This is a PWA</h1>
+  <h1>This is a work in progress application for Apes Together Strong community</h1>
 );
 
 export default About;
